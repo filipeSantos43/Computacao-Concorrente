c5ef1c0579f9484ee89c126acbb6ac32b30449ed
filipeSantos43 <45341464+filipeSantos43@users.noreply.github.com>
2022-05-05 23:23:57 -0300

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,40 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<pthread.h>
+#include "timer.h"
 
-
-long int tam_vet;  
-int nthreads;  
+long int tam_vet; 
+int nthreads; 
 float *vet; 
-float *maxLocal;
-float *minLocal;
-
+float maxS;
+float minS;
 
 typedef struct{
-   int id; 
-   int tam_vet;
-   float min;
-   float max;
+   float max; 
+   float min; 
 } tArgs;
 
+//fluxo das threads
 void * tarefa(void * arg) {
-    tArgs *args = (tArgs*) arg;
-    float min = vet[0]; 
-    float max = vet[0];
-    
-    for(int i = args->id * (tam_vet/nthreads); i < (args->id + 1) * (tam_vet/nthreads); i++){
-        if (min > vet[i]) {
-            min = vet[i];
-        }
-        if (max < vet[i]) {
-           max = vet[i];   
-         }
-         minLocal[(args->id)++] = min;
-         maxLocal[(args->id)++] = max;
-    }
+    long int id = (long int) arg; 
+    tArgs *args = malloc(sizeof(tArgs));
+   
+    long int fim; 
+    if(id == nthreads-1) 
+        fim = tam_vet;
+    else 
+        fim = (id * tam_vet/nthreads) + tam_vet/nthreads; 
 
+    args->max=vet[id * tam_vet/nthreads];
+    args->min=vet[id * tam_vet/nthreads];
     
-    pthread_exit(NULL); 
+   
+    for(long int i=(id * tam_vet/nthreads); i<fim; i++){
+        if(vet[i]>args->max){
+            args->max = vet[i];
+        }
+        if(vet[i]<args->min){
+            args->min = vet[i];
+        }
+   }
+     
+   pthread_exit((void *) args); 
 }
 
 void seqMaxMin(int tam_vet){
@@ -49,19 +53,27 @@ void seqMaxMin(int tam_vet){
             max = vet[i];        
         }
     }
-    printf("Execução Sequencial: Minimo = %.5f e Maximo = %.5f \n", min, max);
+    
+    maxS = max;
+    minS = min;
+   // printf("Execução Sequencial: Minimo = %.5f e Maximo = %.5f \n", min, max);
 }
 
 int main(int argc, char *argv[]) {
-	
-	if(argc < 3) {
-       fprintf(stderr, "Digite: %s <dimensao do vetor> <numero threads>\n", argv[0]);
-       return 1; 
-	}
-	
-	tam_vet = atoll(argv[1]); 
-	nthreads = atoi(argv[2]);
-	
+    double ini, fim, tempoC, tempoS;
+    float  maxCon, minCon; 
+    pthread_t *tid; 
+    tArgs *retorno; 
+
+    if(argc < 3) {
+        fprintf(stderr, "Digite: %s <tamanho vetor> <numero threads>\n", argv[0]);
+        return 1; 
+    }
+    tam_vet = atoi(argv[1]);
+    nthreads = atoi(argv[2]);
+
+
+
 	vet = (float*) malloc(sizeof(float)*tam_vet);
 	
 	srand(time(NULL));
@@ -69,57 +81,55 @@ int main(int argc, char *argv[]) {
 	for(int i=0;i<tam_vet;i++){
 		float num = (rand()%1000)*0.8f;
 		vet[i] = num;
-	}
-	
-	for(int i=0;i<tam_vet;i++){
-		printf("%.5f\n", vet[i]);
-	}
-	
+	}       
+        
+    
+    GET_TIME(ini);
 	seqMaxMin(tam_vet);
-	
-			
-	pthread_t *tid; 
-	tArgs *args;
-	
-	tid = (pthread_t*) malloc(sizeof(pthread_t)*nthreads);
-    if(tid==NULL) {puts("ERRO--malloc"); return 2;}
-	args = (tArgs*) malloc(sizeof(tArgs)*nthreads);
-	if(args==NULL) {puts("ERRO--malloc"); return 2;}
-	
-		printf("12");
-	
-	maxLocal =(float*) malloc(sizeof(float)*nthreads);	
-	minLocal = (float*) malloc(sizeof(float)*nthreads);
-	
-	printf("13");
-	
-	 for(int i=0; i<nthreads; i++) {
-      (args+i)->id = i;
-      if(pthread_create(tid+i, NULL, tarefa, (void*) (args+i))){
-         puts("ERRO--pthread_create"); return 3;
-      }
-   } 
-   
-    for(int i=0; i<nthreads; i++) {
-      pthread_join(*(tid+i), NULL);
-   }
-   
-   float maxTotal = 0.0f;
-   float minTotal =0.0f;
-   for (int i = 0; i < nthreads; i++) {
-        if (maxLocal[i] > maxTotal){
-            maxTotal = maxLocal[i];
-		}
-		 if (minLocal[i] < minTotal){
-            minTotal = minLocal[i];
-		}
+    GET_TIME(fim);
+    tempoS=fim-ini;
+    printf("Tempo sequencial:  %lf\n", fim-ini);
+
+    
+    GET_TIME(ini);
+    maxCon=vet[0];
+    minCon=vet[0];
+
+    tid = (pthread_t *) malloc(sizeof(pthread_t) * nthreads);
+ 
+    for(long int i=0; i<nthreads; i++) {
+        if(pthread_create(tid+i, NULL, tarefa, (void*) i)){
+            fprintf(stderr, "ERRO--pthread_create\n");
+            return 3;
+        }
     }
-   
-   printf("Execução Concorrente: Minimo = %.5f e Maximo = %.5f \n", minTotal, maxTotal);
-   free(vet);
-   free(args);
-   free(tid);
-   
-   return 0;
-   
+	
+
+    for(long int i=0; i<nthreads; i++) {
+        if(pthread_join(*(tid+i), (void**) &retorno)){
+            fprintf(stderr, "ERRO--pthread_create\n");
+            return 3;
+        }
+        
+
+        if(retorno->max>maxCon)
+        maxCon=retorno->max;
+        if(retorno->min<minCon)
+        minCon=retorno->min;    
+
+    }
+    GET_TIME(fim);
+    tempoC=fim-ini;
+    printf("Tempo concorrente:  %lf\n", fim-ini);
+
+
+      
+    printf("A execução concorrente é %lf vezes melhor do que a sequencial\n", tempoS/tempoC);
+
+    free(vet);
+    free(tid);
+
+    return 0;
 }
+
+   
